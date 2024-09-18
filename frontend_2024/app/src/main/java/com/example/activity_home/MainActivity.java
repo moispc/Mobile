9ab963605a429cb6ee77ff7ef636504020c3dfb6
Juan Ledesma <juanjoseledesma90@gmail.com>
2024-09-18 15:10:38 -0300

    corrección

--- frontend_2024/app/src/main/java/com/example/activity_home/MainActivity.java
+++ frontend_2024/app/src/main/java/com/example/activity_home/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.food_front.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
