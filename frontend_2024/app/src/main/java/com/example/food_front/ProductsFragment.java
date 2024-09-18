9a6cba3dfd5e3f88ca8c6ae698494891ed5f1209
cerveux <diegomartinguzman@hotmail.com.ar>
2024-09-18 15:50:21 -0300

    Fix(): Merge

--- frontend_2024/app/src/main/java/com/example/food_front/ProductsFragment.java
+++ frontend_2024/app/src/main/java/com/example/food_front/ProductsFragment.java
@@ -8,11 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProductsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ProductsFragment extends Fragment {
 
     public ProductsFragment() {
