5bfd87d6da697be897e8038cfeffe31df51752bb
cerveux <diegomartinguzman@hotmail.com.ar>
2024-09-18 10:56:26 -0300

    build(): checkout

--- frontend_2024/app/src/main/java/com/example/food_front/MainActivity.java
+++ frontend_2024/app/src/main/java/com/example/food_front/MainActivity.java
@@ -47,24 +47,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
-
     }
 
     public void mostrarHome() {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container_view, new HomeFragment()).commit();
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.replace(R.id.fragment_container_view, new HomeFragment());
+        fragmentTransaction.commit();
     }
 
     // You can add methods to show other fragments like SearchFragment or ProfileFragment
     public void mostrarPerfil() {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container_view, new LoginFragment()).commit();
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.replace(R.id.fragment_container_view, new LoginFragment());
+        fragmentTransaction.commit();
     }
 
 }
