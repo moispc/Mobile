64ae0619bf4c6adee73818ccedcf4a45031e41e0
lucasGelpi <lucasglp32@gmail.com>
2024-10-18 00:46:27 -0300

    Pantalla EditProfile sin funcionalidad

--- frontend_2024/app/src/main/java/com/example/food_front/ProfileFragment.java
+++ frontend_2024/app/src/main/java/com/example/food_front/ProfileFragment.java
@@ -1,15 +1,13 @@
 package com.example.food_front;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
 
 public class ProfileFragment extends Fragment {
 
@@ -21,6 +19,24 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflar el layout para este fragmento
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // Encontrar el TextView de "Datos personales"
+        TextView personalData = view.findViewById(R.id.personal_data);
+
+        // Agregar un onClickListener para navegar a "Editar perfil"
+        personalData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Iniciar la transacción para mostrar el fragmento de "Editar perfil"
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container_view, new EditProfileFragment());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return view;
     }
 }
