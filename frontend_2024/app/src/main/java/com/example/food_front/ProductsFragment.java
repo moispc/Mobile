d5944dd28320faddc87c0ebe01dd686be394f54b
Juan Ledesma <juanjoseledesma90@gmail.com>
2024-10-21 19:10:25 -0300

    prueba de conexión con endpoint de producto

--- frontend_2024/app/src/main/java/com/example/food_front/ProductsFragment.java
+++ frontend_2024/app/src/main/java/com/example/food_front/ProductsFragment.java
@@ -6,10 +6,26 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 public class ProductsFragment extends Fragment {
@@ -26,9 +42,8 @@ public class ProductsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_products, container, false);
 
         ImageView firstImageView = view.findViewById(R.id.imageView1);
-        ImageView secondImageView = view.findViewById(R.id.imageView2);
-        ImageView thirdImageView = view.findViewById(R.id.imageView3);
 
+        getProductList();
 
         firstImageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,30 +53,58 @@ public class ProductsFragment extends Fragment {
             }
         });
 
-        secondImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Replace fragment on button click
-                replaceFragment(new SelectedProductFragment());  // Replace with another fragment
-            }
-        });
-
-        thirdImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Replace fragment on button click
-                replaceFragment(new SelectedProductFragment());  // Replace with another fragment
-            }
-        });
-
-
-
-
-
-
 
         return view;
     }
+
+
+    private void getProductList() {
+        String url = "https://backmobile1.onrender.com/api/producto/";
+        Log.d("Lista de productos", "URL: " + url);
+
+        // Crear el request de volley
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // Mostrar toda la respuesta por consola
+                        Log.d("Product Response", response.toString());
+
+                        // Procesar la respuesta aquí
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject product = response.getJSONObject(i);
+                                String productName = product.getString("nombre_producto");
+                                String prodctPrice = product.getString("precio");
+                                String productDescription = product.getString("descripcion");
+                                String productImage = product.getString("imageURL");
+
+                                Log.d("Product Name", productName);
+                            }
+
+                            Toast.makeText(getContext(), "Lista cargada", Toast.LENGTH_SHORT).show();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getContext(), "Invalid response from server", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Volley Error", "Error: " + error.toString());
+                if (error.networkResponse != null) {
+                    Log.e("Volley Error", "Status Code: " + error.networkResponse.statusCode);
+                }
+                Toast.makeText(getContext(), "Error en la carga de datos: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Agregar la request a la queue de Volley
+        RequestQueue queue = Volley.newRequestQueue(requireContext());
+        queue.add(request);
+    }
+
 
     private void replaceFragment(Fragment newFragment) {
         // Get the FragmentManager and start a transaction
--- frontend_2024/app/src/main/java/com/example/food_front/ProfileFragment.java
+++ frontend_2024/app/src/main/java/com/example/food_front/ProfileFragment.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.food_front.utils.SessionManager;
+
 public class ProfileFragment extends Fragment {
+    private SessionManager sessionManager;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -21,8 +27,15 @@ public class ProfileFragment extends Fragment {
         // Inflar el layout para este fragmento
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        // Inicializar las vistas
+        TextView btnLogOut = view.findViewById(R.id.logout);
+
         // Encontrar el TextView de "Datos personales"
         TextView personalData = view.findViewById(R.id.personal_data);
+
+        // Inicializar SessionManager
+        sessionManager = new SessionManager(requireContext());
+
 
         // Agregar un onClickListener para navegar a "Editar perfil"
         personalData.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +45,23 @@ public class ProfileFragment extends Fragment {
                 FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 fragmentTransaction.replace(R.id.fragment_container_view, new EditProfileFragment());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        // Agrego un OnClickListener al botón de cerrar sesión
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Iniciar la transacción para mostrar el fragmento de "Login" luego de eliminar el token
+                sessionManager.clearSession();
+
+                Log.d("Logout", "Logout button clicked");
+
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container_view, new LoginFragment());
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
             }
